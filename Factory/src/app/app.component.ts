ddbd9e72e44840c5a4a0a1a335dede7bce3c7d8c
QQuyen <daoquyen.073098@gmail.com>
2022-10-21 17:07:42 +0700

    update html

--- Factory/src/app/app.component.ts
+++ Factory/src/app/app.component.ts
@@ -25,6 +25,17 @@ export class AppComponent {
                       'Denouncing pleasure and praising pain was born and I will give you a complete, great explorer.',
                       
                     ];
+
+why_content_title1 = ['Award Winning',
+                    '800+ Sucessful Project',
+                    'Professional Workers',
+                    
+                  ];
+why_content_des1 = ['Complete account of the system, and expound the actual teachings of the great explorer of the truth.',
+                  'Master builder of human happiness no one rejects, dislikes, or avoids pleasure itself.',
+                  'Know how to pursue pleasure explain tconsequences that are ut extremely seds consequences.',
+                  
+                ];
   
   constructor(){
     this.bannerInput = this.bannerDetail[0];
